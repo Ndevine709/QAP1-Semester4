4f4546741ce7e5cd40effb8cfba62ba1036abb8e
Noah Devine <Noah.Devine@keyin.com>
2025-05-26 16:08:30 -0230

    - Created basic project structure - Added basic code for the Address class needed for this banking app.

---
+++ simple-banking-app/src/main/java/com/noahd/banking/Address.java
@@ -0,0 +1,35 @@
+package com.noahd.banking;
+
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Address (String street, String city, String state, String zip) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String toString () {
+        return (this.street + ", " + this.city + ", " + this.state + " " + this.zip);
+    }
+}
