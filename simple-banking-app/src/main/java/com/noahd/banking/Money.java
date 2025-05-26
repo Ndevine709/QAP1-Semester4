c88257e9a8ee79bcbf023f71a0f78e4330380b8b
Noah Devine <Noah.Devine@keyin.com>
2025-05-26 16:22:58 -0230

    - Added basic setup for the Money class.

---
+++ simple-banking-app/src/main/java/com/noahd/banking/Money.java
@@ -0,0 +1,56 @@
+package com.noahd.banking;
+
+public class Money {
+
+    private long dollars;
+    private long cents;
+
+
+    public Money(double amount) {
+        this.dollars = (long) amount;
+        this.cents = Math.round((amount - dollars) * 100);
+    }
+
+    public Money(Money otherObject) {
+        this.dollars = otherObject.dollars;
+        this.cents = otherObject.cents;
+    }
+
+
+    public Money add(Money otherAmount) {
+        long totalCents = this.cents + otherAmount.cents;
+        long totalDollars = this.dollars + otherAmount.dollars + totalCents / 100;
+
+        totalCents = totalCents % 100;
+        return new Money(totalDollars + totalCents / 100.0);
+    }
+
+    public Money subtract(Money otherAmount) {
+        long totalCents = this.cents - otherAmount.cents;
+        long totalDollars = this.dollars - otherAmount.dollars;
+
+        if (totalCents < 0) {
+            totalCents += 100;
+            totalDollars -= 1;
+        }
+        return new Money(totalDollars + totalCents / 100.0);
+    }
+
+    public int compareTo(Money otherObject) {
+        if (this.dollars != otherObject.dollars) {
+            return Long.compare(this.dollars, otherObject.dollars);
+        } else {
+            return Long.compare(this.cents, otherObject.cents);
+        }
+    }
+
+    public boolean equals(Money otherObject) {
+        if (this == otherObject) return true;
+        if (otherObject == null) return false;
+        return this.dollars == otherObject.dollars && this.cents == otherObject.cents;
+    }
+
+    public String toString() {
+        return String.format("$%d.%02d", dollars, cents);
+    }
+}
