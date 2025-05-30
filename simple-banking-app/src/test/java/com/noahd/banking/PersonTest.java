8ac5b1e34b7b9bb49c26a58baa94b3e17690a037
Noah Devine <Noah.Devine@keyin.com>
2025-05-30 13:56:40 -0700

    Added unit test for Person class
    - Added unit tests for the Person class.

--- simple-banking-app/src/test/java/com/noahd/banking/PersonTest.java
+++ simple-banking-app/src/test/java/com/noahd/banking/PersonTest.java
@@ -1,4 +1,27 @@
 package com.noahd.banking;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
 public class PersonTest {
+
+    @Test
+    void testPersonInitialization() {
+        Address testAddress = new Address("123 George St", "St. John's", "NL", "A1A2R2");
+        Person testPerson = new Person("Snake", "Solid", testAddress);
+
+        assertEquals("Snake", testPerson.getLastName());
+        assertEquals("Solid", testPerson.getFirstName());
+        assertEquals(testAddress, testPerson.getHome());
+    }
+
+    @Test
+    void testNullAddress() {
+         Person testPerson = new Person("Snake", "Solid", null);
+
+         assertNull(testPerson.getHome());
+         assertEquals("Solid Snake, null", testPerson.toString());
+    }
 }
