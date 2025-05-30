8e45347d4d6a692482c1161e8ded4c658ec55ec4
Noah Devine <Noah.Devine@keyin.com>
2025-05-30 13:17:08 -0700

    Created unit tests for the Address class
    - Created two unit tests for the Address java class.

--- simple-banking-app/src/test/java/com/noahd/banking/AddressTest.java
+++ simple-banking-app/src/test/java/com/noahd/banking/AddressTest.java
@@ -1,4 +1,26 @@
 package com.noahd.banking;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class AddressTest {
+
+    @Test
+    void testAddressInitialization() {
+        Address testAddress = new Address("123 George St", "St. John's", "NL", "A1A2R2");
+
+        assertEquals("123 George St", testAddress.getStreet());
+        assertEquals("St. John's", testAddress.getCity());
+        assertEquals("NL", testAddress.getState());
+        assertEquals("A1A2R2", testAddress.getZip());
+    }
+
+    @Test
+    void testToStringMethod() {
+        Address testAddress = new Address("123 George St", "St. John's", "NL", "A1A2R2");
+
+        String expectedOutcome = "123 George St, St. John's, NL A1A2R2";
+
+        assertEquals(expectedOutcome, testAddress.toString());
+    }
 }
