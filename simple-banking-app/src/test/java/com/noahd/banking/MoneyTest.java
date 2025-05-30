fa0bc1e7624c12269220b0ae288a491ee0b74cfc
Noah Devine <Noah.Devine@keyin.com>
2025-05-30 14:16:20 -0700

    Added unit tests for Money class
    - Created unit tests for the Money class.

--- simple-banking-app/src/test/java/com/noahd/banking/MoneyTest.java
+++ simple-banking-app/src/test/java/com/noahd/banking/MoneyTest.java
@@ -1,4 +1,38 @@
 package com.noahd.banking;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class MoneyTest {
+    
+    @Test
+    void testAddMoney() {
+        Money testMoney1 = new Money(10.50);
+        Money testMoney2 = new Money(5.00);
+        Money result = testMoney1.add(testMoney2);
+
+        assertEquals("$15.50", result.toString());
+    }
+
+    @Test
+    void testSubtractMoney() {
+        Money testMoney1 = new Money(10.50);
+        Money testMoney2 = new Money(5.00);
+        Money result = testMoney1.subtract(testMoney2);
+
+        assertEquals("$5.50", result.toString());
+    }
+
+    @Test
+    void testIsEqualTo() {
+        Money testMoney1 = new Money(10.50);
+        Money testMoney2 = new Money(10.50);
+        Money testMoney3 = new Money(9.99);
+
+        assertTrue(testMoney1.isEqualTo(testMoney2));
+        assertFalse(testMoney1.isEqualTo(testMoney3));
+    }
 }
